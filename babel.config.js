6592b360a3a14c836f6fe59e36e414598a219a7c
Zhongxing Guo <Gorrister_8@hotmail.com>
2018-10-08 21:24:37 +1300

    Upgraded packages.

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-flow',
+    ],
+    plugins: [
+        'react-hot-loader/babel',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-import-meta',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-json-strings',
+        [
+            '@babel/plugin-proposal-decorators',
+            {
+                legacy: true,
+            },
+        ],
+        '@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-throw-expressions',
+    ],
+};
